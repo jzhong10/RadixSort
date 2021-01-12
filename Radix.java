beb713cb4f05903fb04274c397927603f7f431df
jzhong10 <junzhong2003@gmail.com>
2021-01-12 17:10:44 -0500

    implemented and tested merge() method for SortableLinkedLists

--- Radix.java
+++ Radix.java
@@ -1,58 +1,6 @@
 public class Radix {
     public static void main(String[] args) {
-      int n = 0;
-      int c = 0;
-      if (args.length>0) {
-        n = Integer.parseInt(args[0]);
-      }
-      if (args.length>1) {
-        c = Integer.parseInt(args[1]);
-      }
-      System.out.println(nth(n, c));
-      System.out.println(nth(n, c));
-      System.out.println(length(n));
-      MyLinkedList og = new MyLinkedList();
-      for (int i = 0; i<5; i++) {
-        og.add(i+"");
-      }
-      System.out.println();
-      System.out.println(og.toString());
-      MyLinkedList[] buc = new MyLinkedList[10];
-      for (int i = 0; i<buc.length; i++) {
-        buc[i] = new MyLinkedList();
-      }
-      buc[0].add("9");
-      buc[0].add("1");
-      buc[1].add("8");
-      System.out.println(buc[0].toString());
-      System.out.println(buc[1].toString());
       
-      og.extend(buc[1]);
-      System.out.println(og.toString());
-      System.out.println(buc[1].toString());
-      System.out.println();
-      og.extend(buc[2]);
-      System.out.println(og.toString());
-      System.out.println(buc[2].toString());
-      //merge(og, buc);
-      
-      System.out.println(og.toString());
-      System.out.println(buc[0].toString());
-      System.out.println(buc[1].toString());
-      
-      System.out.println();
-      merge(og, buc);
-      System.out.println(og.toString());
-      System.out.println("of course it starts randomly working");
-      og = new MyLinkedList();
-      buc = new MyLinkedList[10];
-      for (int i = 0; i<buc.length; i++) {
-        buc[i] = new MyLinkedList();
-        buc[i].add(i+"");
-        buc[i].add(i*10+"");
-      }
-      merge(og, buc);
-      System.out.println(og.toString());
     }
     
     public static int nth(int n, int col) {
@@ -68,6 +16,13 @@ public class Radix {
     }
     
     public static void merge(MyLinkedList original, MyLinkedList[] buckets) {
+      for (int i = 0; i<buckets.length; i++) {
+        // /System.out.println(buckets[i].toString());
+        original.extend(buckets[i]);
+      }
+    }
+    
+    public static void merge(SortableLinkedList original, SortableLinkedList[] buckets) {
       for (int i = 0; i<buckets.length; i++) {
         // /System.out.println(buckets[i].toString());
         original.extend(buckets[i]);
---
+++ RadixTester.java
@@ -0,0 +1,73 @@
+public class RadixTester {
+    public static void main(String[] args) {
+      int n = 0;
+      int c = 0;
+      if (args.length>0) {
+        n = Integer.parseInt(args[0]);
+      }
+      if (args.length>1) {
+        c = Integer.parseInt(args[1]);
+      }
+      /*
+      System.out.println(Radix.nth(n, c));
+      System.out.println(Radix.nth(n, c));
+      System.out.println(Radix.length(n));
+      MyLinkedList og = new MyLinkedList();
+      for (int i = 0; i<5; i++) {
+        og.add(i+"");
+      }
+      System.out.println();
+      System.out.println(og.toString());
+      MyLinkedList[] buc = new MyLinkedList[10];
+      for (int i = 0; i<buc.length; i++) {
+        buc[i] = new MyLinkedList();
+      }
+      buc[0].add("9");
+      buc[0].add("1");
+      buc[1].add("8");
+      System.out.println(buc[0].toString());
+      System.out.println(buc[1].toString());
+      
+      og.extend(buc[1]);
+      System.out.println(og.toString());
+      System.out.println(buc[1].toString());
+      System.out.println();
+      og.extend(buc[2]);
+      System.out.println(og.toString());
+      System.out.println(buc[2].toString());
+      //merge(og, buc);
+      
+      System.out.println(og.toString());
+      System.out.println(buc[0].toString());
+      System.out.println(buc[1].toString());
+      
+      System.out.println();
+      Radix.merge(og, buc);
+      System.out.println(og.toString());
+      System.out.println("of course it starts randomly working");
+      og = new MyLinkedList();
+      buc = new MyLinkedList[10];
+      og.add("it all starts here");
+      for (int i = 0; i<buc.length; i++) {
+        buc[i] = new MyLinkedList();
+        buc[i].add(i+"");
+        buc[i].add(i*10+"");
+      }
+      Radix.merge(og, buc);
+      System.out.println(og.toString());
+      */
+      SortableLinkedList orig = new SortableLinkedList();
+      SortableLinkedList[] bucket = new SortableLinkedList[10];
+      orig.add(100);
+      System.out.println(orig.toString());
+      for (int i = 0; i<bucket.length; i++) {
+        bucket[i] = new SortableLinkedList();
+        if (i<6) {
+          bucket[i].add(i);
+          bucket[i].add(i);
+        }
+      }
+      Radix.merge(orig, bucket);
+      System.out.println(orig.toString());
+    }
+  }
